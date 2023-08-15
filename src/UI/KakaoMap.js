44d7a3b24e36b0c6da662b718ce5c1194e7221b0
kinwjdghks <kinwjdghks@naver.com>
2023-08-15 14:55:23 +0900

    before modifying useEffect in Kakaomap

--- src/UI/KakaoMap.js
+++ src/UI/KakaoMap.js
@@ -1,27 +1,72 @@
-import { useRef,useEffect } from "react";
+import { useRef,useEffect,useState } from "react";
 const {kakao} = window;
-const Kakaomap = ({place}) =>{
-    const placeList = {
-        '강남':[37.49798640766797,127.02764966969421],
-        '신논현':[37.50450137396596,127.02450799203919]
-    };
+const Kakaomap = ({place,searchinput}) =>{
     
+    const [selectedCoor,setSelectedCoor] = useState([]);
+    
+   
+    const infowindow = new kakao.maps.InfoWindow({zIndex:1}); //장소명 나타내는 인포윈도우
+    const ps = new kakao.maps.services.Places(); //장소 검색 객체
     const container = useRef();
-
     useEffect(()=>{
-        const pos =  (placeList[place]==null) ? new kakao.maps.LatLng(placeList['강남'][0],placeList['강남'][1]): new kakao.maps.LatLng(placeList[place][0],placeList[place][1]);
+        
+        const placesSearchCB =(data, status, pagination) =>{
+            if (status === kakao.maps.services.Status.OK) {
+        
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                // LatLngBounds 객체에 좌표를 추가합니다
+                let bounds = new kakao.maps.LatLngBounds();
+        
+                for (let i=0; i<data.length; i++) {
+                    displayMarker(data[i]);    
+                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }       
+        
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+                map.setBounds(bounds);
+            } 
+        }
+        
+        const displayMarker = (place) => {
+        
+            // 마커를 생성하고 지도에 표시합니다
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x) 
+            });
+        
+            // 마커에 클릭이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'click', function() {
+                // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+                infowindow.open(map, marker);
+                setSelectedCoor({Lat: place.y, Lng:place.x});
+            });
+        }
+        
+
+        
+
+        const pos =  (place==null) ? new kakao.maps.LatLng(37.49798640766797,127.02764966969421) : new kakao.maps.LatLng(place[0],place[1]);
         const options = {
-            // center: new kakao.maps.LatLng(placeList['강남']),
             center: pos,
             level: 6
         }
-        const marker = new kakao.maps.Marker({
-            position: pos
-        });
-        
         const map = new kakao.maps.Map(container.current, options);
+
+        if(place!=null){
+        const marker = new kakao.maps.Marker({position: pos});
         marker.setMap(map);
-    },[])
+        }
+
+        else{
+            setTimeout(()=>{
+                ps.keywordSearch({searchinput}, placesSearchCB); 
+
+            },1000);
+
+        }
+    },[place,searchinput])
 
     return <div style={{width:'100%',height:'15rem'}} ref={container}></div>;
 }
--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -3,50 +3,56 @@ import Card from "../UI/Card";
 import Kakaomap from "../UI/KakaoMap";
 import snowboard from "../Images/snowboard.jpeg";
 import Modal from "../UI/Modal";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+
+const dummyList = [
+  {
+    meetingType: "discussion",
+    place: "강남",
+    date: new Date("2023-08-27"),
+    theme: "경제",
+    image: "",
+    point:[37.49798640766797,127.02764966969421],
+    key: "1",
+  },
+  // {
+  //   meetingType: "discussion",
+  //   place: "신논현",
+  //   date: new Date("2023-09-10"),
+  //   theme: "문학",
+  //   image: "",
+  //   point:[37.50450137396596,127.02450799203919],
+  //   key: "2",
+  // },
+  {
+    meetingType: "activity",
+    place: "강원도 비발디파크",
+    date: "미정",
+    theme: "스키여행",
+    image: snowboard,
+    point: null,
+    key: "3",
+  },
+];
+
 const Activity = () => {
-  const dummyList = [
-    {
-      meetingType: "discussion",
-      place: "강남",
-      date: new Date("2023-08-27"),
-      theme: "경제",
-      image:"",
-      key:'1'
-    },
-    {
-      meetingType: "discussion",
-      place: "신논현",
-      date: new Date("2023-09-10"),
-      theme: "문학",
-      image:"",
-      key:'2'
-    },
-    {
-      meetingType: "activity",
-      place: "강원도 비발디파크",
-      date: "미정",
-      theme: "스키여행",
-      image: snowboard,
-      key:'3'
-    },
-  ];
-  const [upcomingList,setUpcomingList] = useState(dummyList);
-  const addNewActivity = (event) =>{
-    setUpcomingList((prev)=>{
-      return [...prev,event];
+  const [upcomingList, setUpcomingList] = useState(dummyList);
+  const addNewActivity = (event) => {
+    setUpcomingList((prev) => {
+      return [...prev, event];
     });
-  }
+  };
   /*입력 form 관련 State들*/
-  // const [inputmeetingType,setinputmeetingType] = useState('discussion');
-  // const [inputDate,setInputDate] = useState(null);
-  // const [inputPlace,setInputPlace] = useState(null);
-  // const [inputImage,setInputImage] = useState(null);
-  // const [inputTheme,setInputTheme] = useState("");
-  const [imageUpload,setImageUpload]= useState(false);
-  
+ 
+  const [kakaoSearchInput, setKakaoSearchInput] = useState("");
+  const kakaoSearchInputChangeHandler = (event) =>
+    setKakaoSearchInput(event.target.value);
+
+  const [imageUpload, setImageUpload] = useState(false);
+  const [selectPlace, setSelectPlace] = useState(false);
+
   const imageUploadStateHandler = (e) => setImageUpload(e.target.checked);
-  
+  const selectPlaceStateHandler = (e) => setSelectPlace(e.target.checked);
 
   const arrDayStr = ["일", "월", "화", "수", "목", "금", "토"];
   const dateStr = (event) => {
@@ -61,10 +67,10 @@ const Activity = () => {
       ")"
     );
   };
-  const [isAddingEvent,setIsAddingEvent] = useState(false);
-  const addNewEventHandler = ()=> setIsAddingEvent(true);
+  const [isAddingEvent, setIsAddingEvent] = useState(false);
+  const addNewEventHandler = () => setIsAddingEvent(true);
   const doneAddingNewEventHandler = () => setIsAddingEvent(false);
-  const formSubmitHandler = (event) =>{
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     console.log(event.target.value);
     // const newEvent = {
@@ -72,50 +78,111 @@ const Activity = () => {
 
     //   key : Math.random().toString(),
     // }
-  }
+  };
 
   return (
     <div className={styles.activityPage}>
       <Card className="upcoming">
         <p style={{ fontWeight: 700, fontSize: "2rem" }}>Upcoming</p>
         <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-          <div className={styles.newEventBtn} onClick={addNewEventHandler}>New Event</div>
+          <div className={styles.newEventBtn} onClick={addNewEventHandler}>
+            New Event
+          </div>
         </div>
-        {isAddingEvent && /* 새 모임 생성하는 양식 */
-        <Card className="information">
-              {/* <Kakaomap/> */}
-              <div style={{width:'100%',height:'15rem',backgroundColor:"red"}}></div>
-              <form onSubmit={formSubmitHandler}>
-                <fieldset>
-                  <div><input name='meetingType' id='discussion' value='discussion' type='radio' defaultChecked/><label htmlFor='discussion'>Discussion</label> 
-                  <input name='meetingType' id='activity' value='activity' type='radio'/><label htmlFor='activity'>Activity</label></div>
-
-                  <div><label htmlFor='meetingDate'>Date: </label><input type='date' name='meetingDate' required min={new Date()}/></div>
-                  <div><label htmlFor='theme'>Theme: </label><input type='text' required/></div>
-                  <div><label htmlFor='image'>Image upload:</label>  <input type='checkbox' name='imageUpload' onChange={imageUploadStateHandler}/>
-                  {imageUpload && <input type='file' accept="image/*"/>}</div>
-                </fieldset>
-                <div className={styles.action}>
-                <button type='submit' >Add</button>
-                <button type='cancel' onClick={doneAddingNewEventHandler}>Cancel</button>
+        {isAddingEvent /* 새 모임 생성하는 양식 */ && (
+          <Card className="information">
+            <form onSubmit={formSubmitHandler} className={styles.newEventForm}>
+              <fieldset>
+                <div className={styles.inputdivs}>
+                  <input
+                    name="meetingType"
+                    id="discussion"
+                    value="discussion"
+                    type="radio"
+                    defaultChecked
+                  />
+                  <label htmlFor="discussion">Discussion</label>
+                  <input
+                    name="meetingType"
+                    id="activity"
+                    value="activity"
+                    type="radio"
+                  />
+                  <label htmlFor="activity">Activity</label>
                 </div>
-              </form>
-            </Card>}
+
+                <div className={styles.inputdivs}>
+                  <label htmlFor="meetingDate">Date: </label>
+                  <input
+                    type="date"
+                    name="meetingDate"
+                    required
+                    min={new Date()}
+                  />
+                </div>
+                <div
+                  className={styles.inputdivs}
+                  style={{ display: "flex", alignItems: "baseline" }}
+                >
+                  <label htmlFor="theme">Theme: </label>
+                  <textarea rows="3" required></textarea>
+                </div>
+                <div className={styles.inputdivs}>
+                  <label htmlFor="image">Select Place: </label>{" "}
+                  <input
+                    type="checkbox"
+                    name="selectPlace"
+                    onChange={selectPlaceStateHandler}
+                  />
+                  {selectPlace && (
+                    <>
+                      <Kakaomap searchinput={kakaoSearchInput} />
+                      <div className ={styles.inputdivs}>
+                        <label htmlFor="kakaoSearch">Search Place: </label>
+                        <input
+                          type="text"
+                          name="kakaoSearch"
+                          onChange={kakaoSearchInputChangeHandler}
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                <div className={styles.inputdivs}>
+                  <label htmlFor="image">Image upload:</label>{" "}
+                  <input
+                    type="checkbox"
+                    name="imageUpload"
+                    onChange={imageUploadStateHandler}
+                  />
+                  {imageUpload && <input type="file" accept="image/*" />}
+                </div>
+                <div className={styles.action}>
+                  <button type="submit">Add</button>
+                  <button type="cancel" onClick={doneAddingNewEventHandler}>
+                    Cancel
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </Card>
+        )}
         {upcomingList.map((event) => {
           return (
             <Card className="information">
-              {(event.image==="") ? null : 
-              <div style={{ width: "100%", height: "15rem" }}>
-                <img
-                  style={{
-                    width: "inherit",
-                    height: "inherit",
-                    objectFit: "fill",
-                  }}
-                  src={event.image}
-                />
-              </div>}
-              <Kakaomap place={event.place} />
+              {event.image === "" ? null : (
+                <div style={{ width: "100%", height: "15rem" }}>
+                  <img
+                    style={{
+                      width: "inherit",
+                      height: "inherit",
+                      objectFit: "fill",
+                    }}
+                    src={event.image}
+                  />
+                </div>
+              )}
+              <Kakaomap place={event.point} />
               {/* <div style={{width:'100%',height:'15rem',backgroundColor:"red"}}></div> */}
               <div className={styles.details}>
                 <p>{event.date === "미정" ? "날짜 미정" : dateStr(event)}</p>
