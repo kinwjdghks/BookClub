2a820603c9ff2d9f51ee952694e2d00ead978659
kinwjdghks <kinwjdghks@naver.com>
2023-08-16 23:56:50 +0900

    image upload 구현

--- src/UI/KakaoMap.js
+++ src/UI/KakaoMap.js
@@ -1,15 +1,23 @@
-import { useRef,useEffect,useState } from "react";
+import { useRef,useEffect } from "react";
 const {kakao} = window;
-const Kakaomap = ({place,searchinput}) =>{
+// {Lat:37.49798640766797,Lng:127.02764966969421}
+
+
+const Kakaomap = ({place,placename,searchinput,onSearch}) =>{
     
-    const [selectedCoor,setSelectedCoor] = useState([]);
-    
-   
     const infowindow = new kakao.maps.InfoWindow({zIndex:1}); //장소명 나타내는 인포윈도우
     const ps = new kakao.maps.services.Places(); //장소 검색 객체
     const container = useRef();
+    
     useEffect(()=>{
         
+        const pos =  (place==null) ? new kakao.maps.LatLng(37.49798640766797,127.02764966969421) : new kakao.maps.LatLng(place.Lat,place.Lng);
+        const options = {
+            center: pos,
+            level: 6
+        }
+        const map = new kakao.maps.Map(container.current, options);
+
         const placesSearchCB =(data, status, pagination) =>{
             if (status === kakao.maps.services.Status.OK) {
         
@@ -40,29 +48,27 @@ const Kakaomap = ({place,searchinput}) =>{
                 // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
                 infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
                 infowindow.open(map, marker);
-                setSelectedCoor({Lat: place.y, Lng:place.x});
+                onSearch({coor:{Lat: place.y, Lng:place.x},name:place.place_name});
+                
             });
         }
         
 
-        
+        if(place!=null){ //주소 있을 때
 
-        const pos =  (place==null) ? new kakao.maps.LatLng(37.49798640766797,127.02764966969421) : new kakao.maps.LatLng(place[0],place[1]);
-        const options = {
-            center: pos,
-            level: 6
-        }
-        const map = new kakao.maps.Map(container.current, options);
-
-        if(place!=null){
         const marker = new kakao.maps.Marker({position: pos});
         marker.setMap(map);
+        kakao.maps.event.addListener(marker, 'click', function() {
+            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + placename + '</div>');
+            infowindow.open(map, marker);
+        });
+
         }
 
         else{
             setTimeout(()=>{
-                ps.keywordSearch({searchinput}, placesSearchCB); 
-
+                ps.keywordSearch(searchinput, placesSearchCB); 
             },1000);
 
         }
--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -2,35 +2,37 @@ import styles from "./Activity.module.css";
 import Card from "../UI/Card";
 import Kakaomap from "../UI/KakaoMap";
 import snowboard from "../Images/snowboard.jpeg";
-import Modal from "../UI/Modal";
-import { useState, useRef, useEffect } from "react";
+import noImage from "../Images/noImage.png";
+// import Modal from "../UI/Modal";
+import { useState,useRef, useEffect } from "react";
 
 const dummyList = [
   {
     meetingType: "discussion",
-    place: "강남",
+    place: {Lat:37.49798640766797,Lng: 127.02764966969421},
+    placename:"강남역",
     date: new Date("2023-08-27"),
     theme: "경제",
-    image: "",
-    point:[37.49798640766797,127.02764966969421],
+    image: null,
     key: "1",
   },
-  // {
-  //   meetingType: "discussion",
-  //   place: "신논현",
-  //   date: new Date("2023-09-10"),
-  //   theme: "문학",
-  //   image: "",
-  //   point:[37.50450137396596,127.02450799203919],
-  //   key: "2",
-  // },
+  {
+    meetingType: "discussion",
+    place: {Lat:37.50450137396596,Lng:127.02450799203919},
+    placename:"신논현역",
+    date: new Date("2023-09-10"),
+    theme: "문학",
+    image: null,
+    key: "2",
+  },
   {
     meetingType: "activity",
-    place: "강원도 비발디파크",
+    place: null,
+    placename: null,
     date: "미정",
     theme: "스키여행",
-    image: snowboard,
-    point: null,
+    // image: snowboard,
+    image:null,
     key: "3",
   },
 ];
@@ -39,20 +41,46 @@ const Activity = () => {
   const [upcomingList, setUpcomingList] = useState(dummyList);
   const addNewActivity = (event) => {
     setUpcomingList((prev) => {
-      return [...prev, event];
+      return [event,...prev];
     });
   };
-  /*입력 form 관련 State들*/
- 
-  const [kakaoSearchInput, setKakaoSearchInput] = useState("");
+  /*입력 form 관련 State/Ref들*/
+  const [meetingType,setMeetingType] = useState('discussion');
+  const meetingTypeStateHandler = (event) =>{
+    setMeetingType(event.target.value);
+  }
+  const ref_date = useRef();
+  const ref_theme = useRef();
+  const imagePreview = useRef();
+
+
+  const [kakaoSearchInput, setKakaoSearchInput] = useState(""); //장소를 검색할 키워드 인풋
   const kakaoSearchInputChangeHandler = (event) =>
     setKakaoSearchInput(event.target.value);
-
+  const [placeSearchOutput, setPlaceSearchOutput] = useState(null);
+  const [placeNameSearchOutput, setPlaceNameSearchOutput] = useState("");
+  const placeSearchOutputHandler = (e) => {
+    setPlaceSearchOutput(e.coor);
+    setPlaceNameSearchOutput(e.name);
+  }
+  
   const [imageUpload, setImageUpload] = useState(false);
-  const [selectPlace, setSelectPlace] = useState(false);
-
   const imageUploadStateHandler = (e) => setImageUpload(e.target.checked);
-  const selectPlaceStateHandler = (e) => setSelectPlace(e.target.checked);
+  const [imageFile,setImageFile] = useState(null);
+  const imageFileHandler = (e) => setImageFile(e.target.files[0]);
+  const [isSelectingPlace, setIsSelectingPlace] = useState(false);
+  const isSelectingPlaceStateHandler = (e) =>{
+    setIsSelectingPlace(e.target.checked);
+    if(e.target.checked===false){
+      setPlaceSearchOutput(null);
+    }
+  }
+  /*이미지 처리 코드*/
+  useEffect(()=>{
+    if(imageFile == null) return;
+    // console.log(imageFile);
+    imagePreview.current.src = URL.createObjectURL(imageFile);
+  },[imageFile]);
 
   const arrDayStr = ["일", "월", "화", "수", "목", "금", "토"];
   const dateStr = (event) => {
@@ -70,14 +98,40 @@ const Activity = () => {
   const [isAddingEvent, setIsAddingEvent] = useState(false);
   const addNewEventHandler = () => setIsAddingEvent(true);
   const doneAddingNewEventHandler = () => setIsAddingEvent(false);
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
-    // const newEvent = {
-    //   meetingType: event.value.
+    
+    // 입력 유효성 확인
+    const input_date = ref_date.current.value;
+    const input_theme = ref_theme.current.value.toString().trim();
+    
+    if(input_theme===''){
+      alert("활동 내용을 입력해주세요.");
+      return;
+    }
+    if(isSelectingPlace && placeSearchOutput==null){
+      alert("약속 장소를 확정해주세요.");
+      return;
+    }
+  
 
-    //   key : Math.random().toString(),
-    // }
+    const newEvent = {
+      meetingType: meetingType,
+      place: placeSearchOutput,
+      placename: placeNameSearchOutput,
+      date: new Date(input_date),
+      theme: input_theme,
+      image: imageFile, 
+      key : Math.random().toString(),
+    }
+    
+    addNewActivity(newEvent);
+    setIsAddingEvent(false);
+    setIsSelectingPlace(false);
+    setKakaoSearchInput("");
+    setImageUpload(false);
+    setImageFile(null);
   };
 
   return (
@@ -85,13 +139,14 @@ const Activity = () => {
       <Card className="upcoming">
         <p style={{ fontWeight: 700, fontSize: "2rem" }}>Upcoming</p>
         <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-          <div className={styles.newEventBtn} onClick={addNewEventHandler}>
+          <div className={`${styles.newEventBtn} ${isAddingEvent ? styles.clicked : ''}`} onClick={addNewEventHandler}>
             New Event
           </div>
-        </div>
-        {isAddingEvent /* 새 모임 생성하는 양식 */ && (
-          <Card className="information">
-            <form onSubmit={formSubmitHandler} className={styles.newEventForm}>
+        </div> 
+        
+        {isAddingEvent && /* 새 모임 생성하는 양식 */
+        (<Card className="information">
+            <form onSubmit={formSubmitHandler} className={styles.newEventForm} method="post" encType="multipart/form-data">
               <fieldset>
                 <div className={styles.inputdivs}>
                   <input
@@ -100,6 +155,7 @@ const Activity = () => {
                     value="discussion"
                     type="radio"
                     defaultChecked
+                    onChange={meetingTypeStateHandler}
                   />
                   <label htmlFor="discussion">Discussion</label>
                   <input
@@ -107,6 +163,7 @@ const Activity = () => {
                     id="activity"
                     value="activity"
                     type="radio"
+                    onChange={meetingTypeStateHandler}
                   />
                   <label htmlFor="activity">Activity</label>
                 </div>
@@ -118,6 +175,7 @@ const Activity = () => {
                     name="meetingDate"
                     required
                     min={new Date()}
+                    ref={ref_date}
                   />
                 </div>
                 <div
@@ -125,26 +183,34 @@ const Activity = () => {
                   style={{ display: "flex", alignItems: "baseline" }}
                 >
                   <label htmlFor="theme">Theme: </label>
-                  <textarea rows="3" required></textarea>
+                  <textarea rows="3" required placeholder={
+                    meetingType==='discussion' ? "책제목과 토론주제를 적어주세요."
+                    : meetingType === 'activity' ? "활동 내용을 적어주세요." : "" 
+                  }
+                  ref={ref_theme}></textarea>
                 </div>
                 <div className={styles.inputdivs}>
                   <label htmlFor="image">Select Place: </label>{" "}
                   <input
                     type="checkbox"
                     name="selectPlace"
-                    onChange={selectPlaceStateHandler}
+                    onChange={isSelectingPlaceStateHandler}
                   />
-                  {selectPlace && (
+                  {isSelectingPlace && (
                     <>
-                      <Kakaomap searchinput={kakaoSearchInput} />
-                      <div className ={styles.inputdivs}>
-                        <label htmlFor="kakaoSearch">Search Place: </label>
+                      <div className={styles.inputdivs}>
+                        <label htmlFor="kakaoSearch">Search: </label>
                         <input
-                          type="text"
+                          type="search"
                           name="kakaoSearch"
                           onChange={kakaoSearchInputChangeHandler}
                         />
                       </div>
+                      <Kakaomap
+                        searchinput={kakaoSearchInput}
+                        onSearch={placeSearchOutputHandler}
+                       
+                      />
                     </>
                   )}
                 </div>
@@ -154,8 +220,10 @@ const Activity = () => {
                     type="checkbox"
                     name="imageUpload"
                     onChange={imageUploadStateHandler}
+                    
                   />
-                  {imageUpload && <input type="file" accept="image/*" />}
+                  {imageUpload && <><input type="file" accept="image/*" onChange={imageFileHandler}/>
+                  <div className={styles.imgPreviewContainer}><img ref={imagePreview} src={noImage} style={{objectFit:"fit", width:'inherit',height:'inherit'}}/></div></>}
                 </div>
                 <div className={styles.action}>
                   <button type="submit">Add</button>
@@ -169,24 +237,24 @@ const Activity = () => {
         )}
         {upcomingList.map((event) => {
           return (
-            <Card className="information">
-              {event.image === "" ? null : (
-                <div style={{ width: "100%", height: "15rem" }}>
+            <Card className="information" key={event.key}>
+              {event.image === null ? null : (
+                <div style={{ width: "100%", height: "15rem", marginBottom:"1rem" }}>
                   <img
                     style={{
                       width: "inherit",
                       height: "inherit",
                       objectFit: "fill",
                     }}
-                    src={event.image}
+                    src={window.URL.createObjectURL(event.image)}
                   />
                 </div>
               )}
-              <Kakaomap place={event.point} />
-              {/* <div style={{width:'100%',height:'15rem',backgroundColor:"red"}}></div> */}
+              {event.place !=null && <Kakaomap place={event.place} onSearch={placeSearchOutputHandler} placename={event.placename}/>}
+              {/* <div style={{width:'100%',height:'15rem',backgroundColor:"green"}}></div> */}
               <div className={styles.details}>
                 <p>{event.date === "미정" ? "날짜 미정" : dateStr(event)}</p>
-                <p>{event.topic}</p>
+                <p>{event.theme}</p>
               </div>
             </Card>
           );
